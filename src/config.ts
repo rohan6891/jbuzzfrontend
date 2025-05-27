c5465f58b1a57e14c7c3c06fd0b27295b59a262c
rohan6891 <rohan5144320@gmail.com>
2025-05-27 15:55:34 +0530

    final commit

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // Centralized config for URLs and important variables
-const API_BASE_URL = "http://localhost:5000/api";
-// const API_BASE_URL = 'https://jbuzzbackend-production.up.railway.app/api';
+// const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = 'https://jbuzzbackend-production.up.railway.app/api';
 
 export const CONFIG = {
   API: {
